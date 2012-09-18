f48a8341991832d131bf92924087bf787615c46a
Mayank Chopra <makchopra@codeaurora.org>
2012-09-18 20:34:16 +0530

    msm_fb: display: Send vsync events using sysfs for MDP3 targets
    Replace sending of vsync timestamp using uevents with sysfs entry
    as uevent result in increase in power numbers due to broadcast in
    nature.
    
    Change-Id: I22cfbd68c8bcbe4a47e94074393f1cbf79920c58
    Signed-off-by: Mayank Chopra <makchopra@codeaurora.org>
    Signed-off-by: Siddhartha Agrawal <agrawals@codeaurora.org>
    
    Conflicts:
    
    	drivers/video/msm/mdp.c
    
    Signed-off-by: Mayank Chopra <makchopra@codeaurora.org>
    
    Conflicts:
    	drivers/video/msm/mdp.c
    	drivers/video/msm/mdp_dma_lcdc.c

--- drivers/video/msm/mdp_dma_lcdc.c
+++ drivers/video/msm/mdp_dma_lcdc.c
@@ -58,6 +58,33 @@ extern boolean mdp_continues_display;
 int first_pixel_start_x;
 int first_pixel_start_y;
 
+static ssize_t vsync_show_event(struct device *dev,
+		struct device_attribute *attr, char *buf)
+{
+	ssize_t ret = 0;
+
+	if (atomic_read(&vsync_cntrl.suspend) > 0 ||
+		atomic_read(&vsync_cntrl.vsync_resume) == 0)
+		return 0;
+
+	INIT_COMPLETION(vsync_cntrl.vsync_wait);
+
+	wait_for_completion(&vsync_cntrl.vsync_wait);
+	ret = snprintf(buf, PAGE_SIZE, "VSYNC=%llu",
+			ktime_to_ns(vsync_cntrl.vsync_time));
+	buf[strlen(buf) + 1] = '\0';
+	return ret;
+}
+
+static DEVICE_ATTR(vsync_event, S_IRUGO, vsync_show_event, NULL);
+static struct attribute *vsync_fs_attrs[] = {
+	&dev_attr_vsync_event.attr,
+	NULL,
+};
+static struct attribute_group vsync_fs_attr_group = {
+	.attrs = vsync_fs_attrs,
+};
+
 int mdp_lcdc_on(struct platform_device *pdev)
 {
 	int lcdc_width;
@@ -116,6 +143,7 @@ int mdp_lcdc_on(struct platform_device *pdev)
 	fbi = mfd->fbi;
 	var = &fbi->var;
 	vsync_cntrl.dev = mfd->fbi->dev;
+	atomic_set(&vsync_cntrl.suspend, 0);
 
 	/* MDP cmd block enable */
 	mdp_pipe_ctrl(MDP_CMD_BLOCK, MDP_BLOCK_POWER_ON, FALSE);
@@ -326,6 +354,21 @@ int mdp_lcdc_on(struct platform_device *pdev)
 		mdp_timer_duration = (HZ);
 	}
 #endif
+
+	if (!vsync_cntrl.sysfs_created) {
+		ret = sysfs_create_group(&vsync_cntrl.dev->kobj,
+			&vsync_fs_attr_group);
+		if (ret) {
+			pr_err("%s: sysfs creation failed, ret=%d\n",
+				__func__, ret);
+			return ret;
+		}
+
+		kobject_uevent(&vsync_cntrl.dev->kobj, KOBJ_ADD);
+		pr_debug("%s: kobject_uevent(KOBJ_ADD)\n", __func__);
+		vsync_cntrl.sysfs_created = 1;
+	}
+
 	return ret;
 }
 
@@ -358,6 +401,10 @@ int mdp_lcdc_off(struct platform_device *pdev)
 #ifndef CONFIG_HUAWEI_KERNEL
 	ret = panel_next_off(pdev);
 #endif
+	atomic_set(&vsync_cntrl.suspend, 1);
+	atomic_set(&vsync_cntrl.vsync_resume, 0);
+	complete_all(&vsync_cntrl.vsync_wait);
+
 	/* delay to make sure the last frame finishes */
 	msleep(16);
 
@@ -372,6 +419,9 @@ void mdp_dma_lcdc_vsync_ctrl(int enable)
 		return;
 
 	spin_lock_irqsave(&mdp_spin_lock, flag);
+	if (!enable)
+		INIT_COMPLETION(vsync_cntrl.vsync_wait);
+
 	vsync_cntrl.vsync_irq_enabled = enable;
 	if (!enable)
 		vsync_cntrl.disabled_clocks = 0;
@@ -387,6 +437,10 @@ void mdp_dma_lcdc_vsync_ctrl(int enable)
 		mdp_enable_irq(MDP_VSYNC_TERM);
 		spin_unlock_irqrestore(&mdp_spin_lock, flag);
 	}
+
+	if (vsync_cntrl.vsync_irq_enabled &&
+		atomic_read(&vsync_cntrl.suspend) == 0)
+		atomic_set(&vsync_cntrl.vsync_resume, 1);
 }
 
 void mdp_lcdc_update(struct msm_fb_data_type *mfd)
