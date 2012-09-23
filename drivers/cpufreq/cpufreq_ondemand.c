2d938d153e0e855672e3cf174ba2b0bd9909f178
Steve Kondik <shade@chemlab.org>
2012-09-23 06:23:39 -0700

    drivers: cpufreq: Add max duration for boosting
     * Don't allow boosting for more than 5 seconds.
    
    Change-Id: Ib602c398ab117ee06c9fa113b1dc9042f1255a8f

--- drivers/cpufreq/cpufreq_ondemand.c
+++ drivers/cpufreq/cpufreq_ondemand.c
@@ -49,6 +49,7 @@
 
 #define DBS_INPUT_EVENT_MIN_FREQ		(810000)
 #define DEFAULT_FREQ_BOOST_TIME			(2500000)
+#define MAX_FREQ_BOOST_TIME			(5000000)
 
 u64 freq_boosted_time;
 
@@ -341,7 +342,7 @@ static ssize_t store_boostpulse(struct kobject *kobj, struct attribute *attr,
 	if (ret < 0)
 		return ret;
 
-	if (input > 1)
+	if (input > 1 && input <= MAX_FREQ_BOOST_TIME)
 		dbs_tuners_ins.freq_boost_time = input;
 	else
 		dbs_tuners_ins.freq_boost_time = DEFAULT_FREQ_BOOST_TIME;
