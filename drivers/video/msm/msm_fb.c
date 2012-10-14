1bdc0679a96e9e8784dfb784346312e8f5de1c06
Daz Jones <yuki@thebronasium.com>
2012-10-14 14:49:11 +0100

    O-oookay... not sure where this came from...

--- drivers/video/msm/msm_fb.c
+++ drivers/video/msm/msm_fb.c
@@ -3225,61 +3225,6 @@ static int msmfb_notify_update(struct fb_info *info, unsigned long *argp)
 	return 0;
 }
 
-static int msmfb_handle_pp_ioctl(struct msm_fb_data_type *mfd,
-						struct msmfb_mdp_pp *pp_ptr)
-{
-	int ret = -1;
-
-	if (!pp_ptr)
-		return ret;
-
-	switch (pp_ptr->op) {
-#ifdef CONFIG_FB_MSM_MDP40
-	case mdp_op_csc_cfg:
-		ret = mdp4_csc_config(&(pp_ptr->data.csc_cfg_data));
-		break;
-
-	case mdp_op_pcc_cfg:
-		ret = mdp4_pcc_cfg(&(pp_ptr->data.pcc_cfg_data));
-		break;
-
-	case mdp_op_lut_cfg:
-		switch (pp_ptr->data.lut_cfg_data.lut_type) {
-		case mdp_lut_igc:
-			ret = mdp4_igc_lut_config(
-					(struct mdp_igc_lut_data *)
-					&pp_ptr->data.lut_cfg_data.data);
-			break;
-
-		case mdp_lut_pgc:
-			ret = mdp4_argc_cfg(
-				&pp_ptr->data.lut_cfg_data.data.pgc_lut_data);
-			break;
-
-		case mdp_lut_hist:
-			ret = mdp_hist_lut_config(
-					(struct mdp_hist_lut_data *)
-					&pp_ptr->data.lut_cfg_data.data);
-			break;
-
-		default:
-			break;
-		}
-		break;
-#endif
-	case mdp_bl_scale_cfg:
-		ret = mdp_bl_scale_config(mfd, (struct mdp_bl_scale_data *)
-				&pp_ptr->data.bl_scale_data);
-		break;
-
-	default:
-		pr_warn("Unsupported request to MDP_PP IOCTL.\n");
-		ret = -EINVAL;
-		break;
-	}
-
-	return ret;
-}
 static int msmfb_handle_metadata_ioctl(struct msm_fb_data_type *mfd,
 				struct msmfb_metadata *metadata_ptr)
 {
