241b523774e74a2fd612f3d375369c29744f0934
Daz Jones <yuki@thebronasium.com>
2013-02-22 17:31:34 +0000

    u8815: determine if a memory increase is needed in runtime
    Change-Id: I0b9f1691564341185da4b4c77b6f174b48e4da44
    
    Signed-off-by: Daz Jones <yuki@thebronasium.com>
    
    Conflicts:
    	arch/arm/configs/cyanogenmod_u8815_defconfig
    	arch/arm/mach-msm/hardware_self_adapt.c

--- arch/arm/mach-msm/hardware_self_adapt.c
+++ arch/arm/mach-msm/hardware_self_adapt.c
@@ -29,6 +29,10 @@ static __u32	frame_buffer_size = 0;
 static __u32	frame_buffer_start = 0;	/* physical start address */
 #endif
 
+#ifdef CONFIG_FRAMEBUF_SELF_ADAPT_HACK
+static bool frame_buffer_boosted = false;
+#endif
+
 /* cust size self adapter */
 static __u32	cust_buffer_size = 0;
 static __u32	cust_buffer_start = 0;	/* physical start address */
@@ -91,12 +95,15 @@ __tagtable(ATAG_CHARGE_FLAG, parse_tag_charge_flag);
 #define ATAG_FRAME_BUFFER_ID 0x4d534D79
 int __init parse_tag_frame_buffer(const struct tag *tags)
 {
+    frame_buffer_size = tags->u.mem.size;
 #ifdef CONFIG_FRAMEBUF_SELF_ADAPT_HACK
-	frame_buffer_size = tags->u.mem.size + 1024*1024;
-#else
-	frame_buffer_size = tags->u.mem.size;
+    if(frame_buffer_size < 6 * 1024 * 1024)
+    {
+        frame_buffer_size += 1024 * 1024;
+        frame_buffer_boosted = true;
+    }
 #endif
-	frame_buffer_start = tags->u.mem.start;
+    frame_buffer_start = tags->u.mem.start;
 	
     printk(KERN_DEBUG "%s: fb addr= 0x%x, size=0x%0x\n", __func__, frame_buffer_start, frame_buffer_size);
     return 0;
@@ -128,13 +135,13 @@ void get_frame_buffer_mem_region(__u32 *start_addr, __u32 *size)
 #define ATAG_CUST_BUFFER_ID 0x4d534D7B
 int __init parse_tag_cust_buffer(const struct tag * tags)
 {
-	cust_buffer_size = tags->u.mem.size;
+    cust_buffer_size = tags->u.mem.size;
+    cust_buffer_start = tags->u.mem.start;
 #ifdef CONFIG_FRAMEBUF_SELF_ADAPT_HACK
-	cust_buffer_start = tags->u.mem.start + 1024*1024;
-#else
-	cust_buffer_start = tags->u.mem.start;
+    if(frame_buffer_boosted)
+        cust_buffer_start += 1024 * 1024;
 #endif
-	
+
     printk(KERN_DEBUG "%s: cust addr= 0x%x, size=0x%0x\n", __func__, cust_buffer_start, cust_buffer_size);
     return 0;
 }
